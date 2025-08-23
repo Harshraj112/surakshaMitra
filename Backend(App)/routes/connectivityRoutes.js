46555cf72a0dbcb3a6a873247113122b1c55a3fd
HARSH RAJ <harshraj250106@gmail.com>
2025-08-23 20:41:00 +0530

    Changing name

--- Backend(App)/routes/connectivityRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { updateConnectivityStatus } from '../controllers/deviceCtrl.js';
-
-const router = express.Router();
-router.post("/connectivity-status", updateConnectivityStatus);
-
-export default router;
--- Backend(App)/utils/getTokenFromHeader.js
+++
@@ -1,9 +0,0 @@
-export const getTokenFromHeader = (req) => {
-  //get token from header
-  const token = req?.headers?.authorization?.split(" ")[1];
-  if (token === undefined) {
-    return "No token found in the header";
-  } else {
-    return token;
-  }
-};
