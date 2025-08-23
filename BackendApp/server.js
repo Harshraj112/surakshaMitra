934ccae9e91b5e1bdd2110392b02ad67e0d022f9
HARSH RAJ <harshraj250106@gmail.com>
2025-08-23 22:02:33 +0530

    Debugging

--- BackendApp/server.js
+++ BackendApp/server.js
@@ -2,7 +2,7 @@ import http from 'http';
 import app from "./app/app.js";
 
 //create the server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(PORT, console.log(`Server is up and running on port ${PORT}`));
 
